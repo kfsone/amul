15e2e226c7edb8fc3c6dd965e4568c073a2dd7f4
Oliver Smith <oliver@kfs.org>
2019-07-28 22:36:09 -0700

    smugl-src building under linux.

--- archive/smugl-src/h/SMUGL.h
+++ archive/smugl-src/h/SMUGL.h
@@ -1,12 +1,12 @@
 /* Build/Release details for ../h/SMUGL */
-#define	SMUGL_BUILD		"0.03.28 (1190728)"
+#define	SMUGL_BUILD		"0.03.29 (1190728)"
 #define	SMUGL_VERSION		0
 #define	SMUGL_REVISION		3
-#define SMUGL_BUILDNO		28
-#define	SMUGL_RELEASE		"0.03.28"
+#define SMUGL_BUILDNO		29
+#define	SMUGL_RELEASE		"0.03.29"
 
 /* Dates */
-#define	SMUGL_DATE		"28/07/2019 21:48"
+#define	SMUGL_DATE		"28/07/2019 22:07"
 #define	SMUGL_BUILT		"Sun, 28 Jul '119"
 #define	SMUGL_BUILD_DATE		"1190728"
 
--- archive/smugl-src/h/SMUGLCOM.h
+++ archive/smugl-src/h/SMUGLCOM.h
@@ -1,12 +1,12 @@
 /* Build/Release details for ../h/SMUGLCOM */
-#define	SMUGLCOM_BUILD		"0.03.28 (1190728)"
+#define	SMUGLCOM_BUILD		"0.03.29 (1190728)"
 #define	SMUGLCOM_VERSION		0
 #define	SMUGLCOM_REVISION		3
-#define SMUGLCOM_BUILDNO		28
-#define	SMUGLCOM_RELEASE		"0.03.28"
+#define SMUGLCOM_BUILDNO		29
+#define	SMUGLCOM_RELEASE		"0.03.29"
 
 /* Dates */
-#define	SMUGLCOM_DATE		"28/07/2019 21:37"
+#define	SMUGLCOM_DATE		"28/07/2019 22:07"
 #define	SMUGLCOM_BUILT		"Sun, 28 Jul '119"
 #define	SMUGLCOM_BUILD_DATE		"1190728"
 
