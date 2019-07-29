799d481aa62745e5494a1b0f53d78508b8b715e5
Oliver Smith <oliver@kfs.org>
2019-07-29 00:15:26 -0700

    Reorganize smugl-src develconfigure

--- archive/smugl-src/develconfigure.sh
+++ archive/smugl-src/develconfigure.sh
@@ -5,7 +5,10 @@
 # If you don't have GCC, you can't use this (atm)
 
 # For linux:
-compiler_flags="-DDEBUG -std=c++14 -O0 -g3 -Wall -Wcast-qual -Wwrite-strings -Wmissing-declarations"
+spam_warnings="-Wold-style-cast -Weffc++"
+warnings="-Wconversion-null  -Wctor-dtor-privacy -Wdelete-non-virtual-dtor  -Wliteral-suffix  -Wmultiple-inheritance -Wnamespaces  -Wnarrowing -Wnoexcept  -Wnoexcept-type  -Wnon-virtual-dtor  -Wreorder  -Wregister -Wstrict-null-sentinel -Wno-non-template-friend  -Woverloaded-virtual  -Wno-pmf-conversions -Wsign-promo  -Wvirtual-inheritance -Wshadow -Wsequence-point -Wuseless-cast"
+sanitizer="-fsanitize=address -fsanitize=undefined -fsanitize=vptr -fsanitize=enum -fsanitize=bool -fsanitize=nonnull-attribute -fsanitize=returns-nonnull-attribute -fsanitize=float-cast-overflow -fsanitize=float-divide-by-zero -fsanitize=object-size -fsanitize=alignment -fsanitize=bounds-strict -fsanitize=signed-integer-overflow -fsanitize=return -fsanitize=null -fsanitize=vla-bound -fsanitize=unreachable -fsanitize=integer-divide-by-zero -fsanitize=shift-base -fsanitize=shift-exponent -fsanitize=shift"
+compiler_flags="-DDEBUG -std=c++17 -O0 -g3 -Wall -Wpedantic -Wcast-qual -Wwrite-strings -Wmissing-declarations $warnings $sanitizer"
 CFLAGS="${compiler_flags}" CXXFLAGS="${compiler_flags}" ./configure
 # If you REALLY want to be pedantic
 #CFLAGS="-DDEBUG -g -Wall -Wshadow -Wstrict-prototypes -Wcast-qual -Wwrite-strings -Wconversion -Wmissing-prototypes -Wmissing-declarations" ./configure
