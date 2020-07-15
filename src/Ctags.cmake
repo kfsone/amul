ae681f3c3e2bbc8be55042f7c04235f96924e106
Oliver Smith <oliver@kfs.org>
2020-07-15 12:32:17 -0700

    Fix ctags building

--- src/Ctags.cmake
+++ src/Ctags.cmake
@@ -1,5 +1,7 @@
 # On Unix, create a target for building the CTAGS file.
 IF ( UNIX )
 	FIND_PROGRAM(CTAGS ctags)
-	ADD_CUSTOM_TARGET(ctags COMMAND ${CTAGS} -R ${CMAKE_SOURCE_DIR}/*/*.cpp ${CMAKE_SOURCE_DIR}/*/*.h)
+	ADD_CUSTOM_TARGET(tags COMMAND ${CTAGS} -R ${CMAKE_SOURCE_DIR})
+	ADD_CUSTOM_TARGET(ctags DEPENDS tags)
+	MESSAGE(STATUS "Enabled ctags")
 ENDIF ( UNIX )
